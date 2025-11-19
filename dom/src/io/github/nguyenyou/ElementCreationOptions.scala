fd43377c2c797c5d825450cb7e7759ffa3f53a25
nguyenyou <tunt081295@gmail.com>
2025-11-19 12:50:20 +0700

    🚢 ship it

---
+++ dom/src/io/github/nguyenyou/ElementCreationOptions.scala
@@ -0,0 +1,7 @@
+package io.github.nguyenyou.dom
+
+import scala.scalajs.js
+
+trait ElementCreationOptions extends js.Object {
+  var is: js.UndefOr[String] = js.undefined
+}
---
+++ dom/src/io/github/nguyenyou/ShadowRootMode.scala
@@ -0,0 +1,8 @@
+package io.github.nguyenyou.dom
+
+opaque type ShadowRootMode <: String = String
+
+object ShadowRootMode {
+  val open: ShadowRootMode = "open"
+  val closed: ShadowRootMode = "closed"
+}
